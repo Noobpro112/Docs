c60b0e3a91719afa32188cd6b36fb6a2c8db5549
Kauan Morais Burger <kauanburger007@gmail.com>
2024-10-05 11:30:54 -0300

    Create bloco_de_notas.sql
    Criando o banco de dados que será utilizado no projeto. A database possui todos os nomes de tabelas e atributos no singular e o nome da tabela em cada atributo para facilitar a compreensão e facilidade de identificação.

---
+++ database/bloco_de_notas.sql
@@ -0,0 +1,131 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 05/10/2024 às 16:29
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `bloco_de_notas`
+--
+CREATE DATABASE IF NOT EXISTS `bloco_de_notas` DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;
+USE `bloco_de_notas`;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tb_documento`
+--
+
+DROP TABLE IF EXISTS `tb_documento`;
+CREATE TABLE `tb_documento` (
+  `id_documento` int(11) NOT NULL,
+  `documento_titulo` varchar(255) DEFAULT NULL,
+  `documento_conteudo` text DEFAULT NULL,
+  `documento_data_criacao` datetime NOT NULL,
+  `documento_data_atualizacao` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `tb_usuario`
+--
+
+DROP TABLE IF EXISTS `tb_usuario`;
+CREATE TABLE `tb_usuario` (
+  `id_usuario` int(11) NOT NULL,
+  `usuario_nome` varchar(255) NOT NULL,
+  `usuario_email` varchar(255) NOT NULL,
+  `usuario_senha` varchar(255) NOT NULL,
+  `usuario_tipo` varchar(255) NOT NULL,
+  `usuario_data_entrada` datetime NOT NULL,
+  `usuario_ativo` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuario_documento`
+--
+
+DROP TABLE IF EXISTS `usuario_documento`;
+CREATE TABLE `usuario_documento` (
+  `id_usuario_documento` int(11) NOT NULL,
+  `id_usuario` int(11) DEFAULT NULL,
+  `id_documento` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `tb_documento`
+--
+ALTER TABLE `tb_documento`
+  ADD PRIMARY KEY (`id_documento`);
+
+--
+-- Índices de tabela `tb_usuario`
+--
+ALTER TABLE `tb_usuario`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- Índices de tabela `usuario_documento`
+--
+ALTER TABLE `usuario_documento`
+  ADD PRIMARY KEY (`id_usuario_documento`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_documento` (`id_documento`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `tb_documento`
+--
+ALTER TABLE `tb_documento`
+  MODIFY `id_documento` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `tb_usuario`
+--
+ALTER TABLE `tb_usuario`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de tabela `usuario_documento`
+--
+ALTER TABLE `usuario_documento`
+  MODIFY `id_usuario_documento` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `usuario_documento`
+--
+ALTER TABLE `usuario_documento`
+  ADD CONSTRAINT `usuario_documento_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `tb_usuario` (`id_usuario`),
+  ADD CONSTRAINT `usuario_documento_ibfk_2` FOREIGN KEY (`id_documento`) REFERENCES `tb_documento` (`id_documento`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
