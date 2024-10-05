b853c56a855833b641e524369d3c693540df1d21
Matheus Yan <reismatheusyan83@gmail.com>
2024-10-05 12:49:24 -0300

    .

---
+++ include/conexao.php
@@ -0,0 +1,7 @@
+<?php
+$hostname="localhost";
+$username="root.Att";
+$password="admin_F9kR3wTz!5Y";
+$database="bloco_de_notas";
+$conexao = mysqli_connect($hostname, $username, $password, $database);
+$conn=mysqli_connect($hostname, $username, $password, $database);
