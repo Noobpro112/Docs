dfe88540f18a1f63c5c971f756695aa6571a3bfb
Kauan Burger <kauan_burguer@estudante.sesisenai.org.br>
2024-10-16 11:54:54 -0300

    Update bloco_de_notas_2.sql
    Adicionando View e tabela documentos

--- database/bloco_de_notas_2.sql
+++ database/bloco_de_notas_2.sql
@@ -2,8 +2,8 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3306
--- Tempo de geração: 16/10/2024 às 00:35
+-- Host: 127.0.0.1
+-- Tempo de geração: 16/10/2024 às 16:53
 -- Versão do servidor: 10.4.32-MariaDB
 -- Versão do PHP: 8.2.12
 
@@ -50,6 +50,23 @@ DELIMITER ;
 -- --------------------------------------------------------
 
 --
+-- Estrutura para tabela `documentos`
+--
+
+DROP TABLE IF EXISTS `documentos`;
+CREATE TABLE `documentos` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(255) NOT NULL,
+  `conteudo` longtext DEFAULT NULL,
+  `data_criacao` datetime DEFAULT current_timestamp(),
+  `data_modificacao` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `ID_Usuario` int(11) NOT NULL,
+  `tamanho_fonte` int(11) DEFAULT 14
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura para tabela `tb_usuario`
 --
 
@@ -76,6 +93,23 @@ INSERT INTO `tb_usuario` (`id_usuario`, `usuario_nome`, `usuario_email`, `usuari
 -- --------------------------------------------------------
 
 --
+-- Estrutura stand-in para view `usuarios_ativos`
+-- (Veja abaixo para a visão atual)
+--
+DROP VIEW IF EXISTS `usuarios_ativos`;
+CREATE TABLE `usuarios_ativos` (
+`id_usuario` int(11)
+,`usuario_nome` varchar(255)
+,`usuario_email` varchar(255)
+,`usuario_senha` varchar(255)
+,`usuario_tipo` enum('ADM','COLLAB')
+,`usuario_data_entrada` datetime
+,`usuario_ativo` tinyint(1)
+);
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura para tabela `usuario_documento`
 --
 
@@ -86,9 +120,25 @@ CREATE TABLE `usuario_documento` (
   `id_documento` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Estrutura para view `usuarios_ativos`
+--
+DROP TABLE IF EXISTS `usuarios_ativos`;
+
+DROP VIEW IF EXISTS `usuarios_ativos`;
+CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `usuarios_ativos`  AS SELECT `tb_usuario`.`id_usuario` AS `id_usuario`, `tb_usuario`.`usuario_nome` AS `usuario_nome`, `tb_usuario`.`usuario_email` AS `usuario_email`, `tb_usuario`.`usuario_senha` AS `usuario_senha`, `tb_usuario`.`usuario_tipo` AS `usuario_tipo`, `tb_usuario`.`usuario_data_entrada` AS `usuario_data_entrada`, `tb_usuario`.`usuario_ativo` AS `usuario_ativo` FROM `tb_usuario` WHERE `tb_usuario`.`usuario_ativo` = 1 ;
+
 --
 -- Índices para tabelas despejadas
 --
+
+--
+-- Índices de tabela `documentos`
+--
+ALTER TABLE `documentos`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Índices de tabela `tb_usuario`
@@ -107,6 +157,12 @@ ALTER TABLE `usuario_documento`
 --
 -- AUTO_INCREMENT para tabelas despejadas
 --
+
+--
+-- AUTO_INCREMENT de tabela `documentos`
+--
+ALTER TABLE `documentos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de tabela `tb_usuario`
