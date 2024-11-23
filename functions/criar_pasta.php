8ae3a70be39219248fb53c41c0e2719a071c0074
Kauan Morais Burger <kauanburger007@gmail.com>
2024-11-23 12:39:15 -0300

    Update criar_pasta.php

--- functions/criar_pasta.php
+++ functions/criar_pasta.php
@@ -13,21 +13,21 @@ if(isset($_POST['nome_pasta']) && !empty($_POST['select_documents'])){
     $CriarPasta -> bind_param('ss', $nome_pasta, $data_hoje);
 
     if($CriarPasta -> execute()){
-        $pasta_id = $conexao -> insert_id;
+        $pasta_id = $conexao->insert_id;
 
         foreach($documentos_selecionados as $id_docs){
-            $AdicionarPasta = $conexao -> prepare("UPDATE documentos SET id_pasta = ? WHERE id= ?");
-            $AdicionarPasta -> bind_param('ii', $pasta_id, $id_docs);
+            $AdicionarPasta = $conexao->prepare("UPDATE documentos SET id_pasta = ? WHERE id= ?");
+            $AdicionarPasta->bind_param('ii', $pasta_id, $id_docs);
+
+            if (!$AdicionarPasta->execute()) {
+                header('Location: ../pgs/admin/home.php?status=failID');
+                exit();
+            }
         }
 
-        if($AdicionarPasta -> execute()){
-            header('Location: ../pgs/admin/home.php?status=success');
-            exit();
-        } else{
-            header('Location: ../pgs/admin/home.php?status=failID');
-            exit();
-        }
-
+        header('Location: ../pgs/admin/home.php?status=success');
+        exit();
+        
     }else{
         header('Location: ../pgs/admin/home.php?status=failIP');
         exit();
