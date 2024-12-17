e47654b18956929fe637fe60044101b5ba88d129
AmandaMallet <amandaramosmallet@gmail.com>
2024-12-17 10:48:32 -0300

    header cadastrar colaborador

--- pgs/admin/cadastrar_colaborador.php
+++ pgs/admin/cadastrar_colaborador.php
@@ -13,33 +13,15 @@
     <script src="https://kit.fontawesome.com/a760d1109c.js" crossorigin="anonymous"></script>
     <style>
         @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap');
+        .headerBusca {
+            visibility: hidden;
+        }
     </style>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 </head>
 
 <body>
-    <header>
-        <div class="headerLogo">
-            <a href="home.php"><img src="../../imgs/logo-branca-t.png" alt="Logo" width="80px"></a>
-        </div>
-        <div class="DropDown" id="menu-icon">
-            <button class="circle" onclick="toggleMenu()">
-                <i class="fa-solid fa-user"></i>
-            </button>
-        </div>
-    </header>
-    <div class="menuDropDown" id="menuDropDown">
-        <p>Colaborador Nome</p> <!--Puxar do banco de dados o nome do colaborador!!!!!-->
-        <a href="#">
-            <i class="fa-solid fa-gear"></i>
-            <label>Configurações</label>
-        </a>
-
-        <a href="../../functions/logout.php">
-            <i class="fa-solid fa-right-from-bracket"></i>
-            <label>Logout</label>
-        </a>
-    </div>
+<?php include_once("../../include/header.php"); ?>
     <main>
         <section class="section1">
             <!--Forms para a box onde o admin vai colocar as informações para cadastro-->
